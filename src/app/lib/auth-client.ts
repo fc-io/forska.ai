a5cda352964ac56aa0ed32ccf7827553319a11e0
Fredrik Carlsson <me@fc.io>
2025-08-09 16:34:21 +0200

    add a clear db script

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -1,5 +1,8 @@
+import {adminClient} from 'better-auth/client/plugins'
 import {createAuthClient} from 'better-auth/solid'
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: 'http://localhost:3000',
+  plugins: [adminClient()],
 })
