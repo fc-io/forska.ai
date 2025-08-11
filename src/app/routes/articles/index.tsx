ea375c3c6ad86b6c6344f15444079e029b2c3286
Fredrik Carlsson <me@fc.io>
2025-08-11 22:32:17 +0200

    update layout

--- src/app/routes/articles/index.tsx
+++ src/app/routes/articles/index.tsx
@@ -4,9 +4,9 @@ import {ArticlesTable} from '../../../components/main/articlesTable'
 
 const Articles = () => {
   return (
-    <div class="min-h-screen bg-background text-foreground flex justify-center p-4">
+    <div class="min-h-screen bg-gray-50 flex justify-center p-4">
       <div class="w-full space-y-8">
-        <div class="bg-card text-card-foreground rounded-sm border shadow-lg p-8">
+        <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-8">
           <ArticlesTable />
         </div>
       </div>
