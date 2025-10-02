7ba8181b45479b33b559eaaf16040537821e3d57
Fredrik Carlsson <me@fc.io>
2025-10-02 14:32:33 +0200

    add article fetch by job seq

--- src/server/cron/judgmentsJobs/judgmentsJobsGetNewArticles.ts
+++ src/server/cron/judgmentsJobs/judgmentsJobsGetNewArticles.ts
@@ -7,20 +7,34 @@ import type {judgmentsJobsGetJobs} from './judgmentsJobsGetJobs.ts'
 
 type Job = Awaited<ReturnType<typeof judgmentsJobsGetJobs>>[number]
 
-const fetchArticlesForJob = async (db: PostgresJsDatabase<typeof schema>, job: Job) => {
+const fetchArticlesForJob = async (db: PostgresJsDatabase<typeof schema>, job: Job, excludeIds: string[]) => {
   const batchSize = Math.max(Math.ceil(job.sendToLLMBatchSize / 3), 1)
   const existingArticleIds = await getProcessingArticleIds(db, job.id)
-  const articleData = await judgmentsJobsCronGetArticles(job.projectId, batchSize, existingArticleIds)
+  const articleData = await judgmentsJobsCronGetArticles(job.projectId, batchSize, [
+    ...existingArticleIds,
+    ...excludeIds,
+  ])
 
   return {...articleData, job}
 }
 
-export const judgmentsJobsGetNewArticles = async (db: PostgresJsDatabase<typeof schema>, allJobs: Job[]) => {
-  const articlesData = await Promise.all(
-    allJobs.map((job) => {
-      return fetchArticlesForJob(db, job)
-    }),
-  )
+const fetchSequentially = async (
+  db: PostgresJsDatabase<typeof schema>,
+  jobs: Job[],
+  acc: Array<Awaited<ReturnType<typeof fetchArticlesForJob>>>,
+  excludeIds: string[],
+): Promise<Array<Awaited<ReturnType<typeof fetchArticlesForJob>>>> => {
+  const [job, ...rest] = jobs
+  return !job
+    ? acc
+    : fetchArticlesForJob(db, job, excludeIds).then((res) => {
+        const nextExclude = res.articlesToJudgeIds.length > 0 ? [...excludeIds, ...res.articlesToJudgeIds] : excludeIds
+        const nextAcc = [...acc, res]
+        return fetchSequentially(db, rest, nextAcc, nextExclude)
+      })
+}
 
-  return articlesData
+export const judgmentsJobsGetNewArticles = async (db: PostgresJsDatabase<typeof schema>, allJobs: Job[]) => {
+  const result = await fetchSequentially(db, allJobs, [], [])
+  return result
 }
