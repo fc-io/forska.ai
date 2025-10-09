4e1b1375d3c1474fbdc2c3101737c0e5bd98e53b
Fredrik Carlsson <me@fc.io>
2025-10-09 10:00:32 +0200

    refactor

--- src/server/routes/ImportRoutes.ts
+++ src/server/routes/ImportRoutes.ts
@@ -5,18 +5,23 @@ import {dataSource} from '../../db/schema.ts'
 import {getDatabase} from '../utils/getDatabase.ts'
 import {withErrorHandler} from '../utils/routeErrorHandler'
 
-export const importRoutes = new Elysia()
-  .use(withErrorHandler())
-  .get('/api/importroutes', async () => {
-    const db = getDatabase()
+export const importRoutes = new Elysia().use(withErrorHandler()).get('/api/importroutes', async () => {
+  const db = getDatabase()
 
-    const rows = await db
-      .select({importRoute: dataSource.importRoute})
-      .from(dataSource)
-      .where(isNotNull(dataSource.importRoute))
-      .groupBy(dataSource.importRoute)
-      .orderBy(asc(dataSource.importRoute))
+  const rows = await db
+    .select({importRoute: dataSource.importRoute})
+    .from(dataSource)
+    .where(isNotNull(dataSource.importRoute))
+    .groupBy(dataSource.importRoute)
+    .orderBy(asc(dataSource.importRoute))
 
-    return {data: rows.map((r) => r.importRoute).filter((v): v is string => Boolean(v))}
-  })
-
+  return {
+    data: rows
+      .map((r) => {
+        return r.importRoute
+      })
+      .filter((v): v is string => {
+        return Boolean(v)
+      }),
+  }
+})
