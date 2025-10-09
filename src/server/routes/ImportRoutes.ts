30cbc047bf2408dc4fa4b0ed0598958cb3887e71
Fredrik Carlsson <me@fc.io>
2025-10-09 10:00:17 +0200

    add import routes route

---
+++ src/server/routes/ImportRoutes.ts
@@ -0,0 +1,22 @@
+import {asc, isNotNull} from 'drizzle-orm'
+import {Elysia} from 'elysia'
+
+import {dataSource} from '../../db/schema.ts'
+import {getDatabase} from '../utils/getDatabase.ts'
+import {withErrorHandler} from '../utils/routeErrorHandler'
+
+export const importRoutes = new Elysia()
+  .use(withErrorHandler())
+  .get('/api/importroutes', async () => {
+    const db = getDatabase()
+
+    const rows = await db
+      .select({importRoute: dataSource.importRoute})
+      .from(dataSource)
+      .where(isNotNull(dataSource.importRoute))
+      .groupBy(dataSource.importRoute)
+      .orderBy(asc(dataSource.importRoute))
+
+    return {data: rows.map((r) => r.importRoute).filter((v): v is string => Boolean(v))}
+  })
+
