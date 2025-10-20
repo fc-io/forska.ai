a5669e875e1cd6294125002b49ebc12063678dca
Fredrik Carlsson <me@fc.io>
2025-10-20 17:35:47 +0200

    update

--- scripts/buildDockerHostnet.ts
+++ scripts/buildDockerHostnet.ts
@@ -7,9 +7,8 @@ await $`docker compose --profile hostnet build api-server-hostnet app-server-hos
 await $`docker compose --profile hostnet push api-server-hostnet app-server-hostnet`
 
 console.log(`\n`)
-console.log(`Built and pushed images to GHCR with tag ${tag}`)
-console.log(`--------------------------`)
+console.log(`Built and pushed images to GHCR with tag ${tag} ----------------`)
 console.log('Now pull images on remote:')
 console.log(`apptainer pull --arch amd64 "$STACK_ROOT/api_server.sif" docker://ghcr.io/$GHCR_OWNER/api-server:${tag}`)
 console.log(`apptainer pull --arch amd64 "$STACK_ROOT/app_server.sif" docker://ghcr.io/$GHCR_OWNER/app-server:${tag}`)
-console.log(`--------------------------`)
+console.log(`----------------------------------------------------------------`)
