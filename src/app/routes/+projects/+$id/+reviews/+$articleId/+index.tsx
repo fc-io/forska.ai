0e75add6f618ae9aa9ca848a11a1cab02b738fc5
Fredrik Carlsson <me@fc.io>
2025-11-03 16:39:48 +0100

    fix types

--- src/app/routes/+projects/+$id/+reviews/+$articleId/+index.tsx
+++ src/app/routes/+projects/+$id/+reviews/+$articleId/+index.tsx
@@ -2,10 +2,13 @@ import {createFileRoute, useNavigate} from '@tanstack/solid-router'
 import {createEffect} from 'solid-js'
 
 const RedirectReviewArticle = () => {
-  const params = Route.useParams() as {id: string; articleId: string}
+  const params = Route.useParams()
   const navigate = useNavigate()
   createEffect(() => {
-    void navigate({to: '/projects/$id/reviews-llm/$articleId', params: {id: params.id, articleId: params.articleId}})
+    void navigate({
+      to: '/projects/$id/reviews-llm/$articleId',
+      params: {id: params().id, articleId: params().articleId},
+    })
   })
   return null
 }
