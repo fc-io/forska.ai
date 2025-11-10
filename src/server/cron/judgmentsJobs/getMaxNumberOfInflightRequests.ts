7d35cd1511a0feea76dbcfebd3f7ce82efbbd2d2
Fredrik Carlsson <me@fc.io>
2025-11-10 21:51:35 +0100

    add more max run req

--- src/server/cron/judgmentsJobs/getMaxNumberOfInflightRequests.ts
+++ src/server/cron/judgmentsJobs/getMaxNumberOfInflightRequests.ts
@@ -3,5 +3,5 @@ import {env} from '../../utils/env.ts'
 export const getMaxNumberOfInflightRequests = (): number => {
   const {SGLANG_MAX_RUNNING_REQUESTS} = env
 
-  return SGLANG_MAX_RUNNING_REQUESTS * 3
+  return SGLANG_MAX_RUNNING_REQUESTS * 6
 }
