faf096073a0dfe68a778d620dbfc656d57864ebf
Fredrik Carlsson <me@fc.io>
2025-11-11 11:20:17 +0100

    chagne max requests

--- src/server/cron/judgmentsJobs/getMaxNumberOfInflightRequests.ts
+++ src/server/cron/judgmentsJobs/getMaxNumberOfInflightRequests.ts
@@ -3,5 +3,5 @@ import {env} from '../../utils/env.ts'
 export const getMaxNumberOfInflightRequests = (): number => {
   const {SGLANG_MAX_RUNNING_REQUESTS} = env
 
-  return SGLANG_MAX_RUNNING_REQUESTS * 6
+  return SGLANG_MAX_RUNNING_REQUESTS * 4
 }
