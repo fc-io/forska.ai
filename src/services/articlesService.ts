a70a4cb19f79e011e330c367af8c78820c16ff69
Fredrik Carlsson <me@fc.io>
2026-01-22 07:54:25 +0100

    fix more lint error

--- src/services/articlesService.ts
+++ src/services/articlesService.ts
@@ -4,7 +4,7 @@ export const fetchArticlesSearch = async (query: string) => {
   const response = await apiClient.api.articles.search.get({query: {q: query}})
 
   if (response.error) {
-    throw new Error(String(response.error.value))
+    throw new Error(JSON.stringify(response.error.value))
   }
 
   return response.data.data
@@ -14,7 +14,7 @@ export const fetchArticleDetails = async (id: string) => {
   const response = await apiClient.api.articles({id}).get()
 
   if (response.error) {
-    throw new Error(String(response.error.value))
+    throw new Error(JSON.stringify(response.error.value))
   }
 
   return response.data
