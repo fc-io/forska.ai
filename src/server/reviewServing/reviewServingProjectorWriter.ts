4ba3b4d1e50c20c54d2f38b4f8656109a2106558
Fredrik Carlsson <me@fc.io>
2026-07-29 20:28:35 +0200

    Centralize selected import current maintenance

--- src/server/reviewServing/reviewServingProjectorWriter.ts
+++ src/server/reviewServing/reviewServingProjectorWriter.ts
@@ -64,7 +64,7 @@ export type ReviewServingProjectorRecord = {
 
 export type DeleteReviewServingProjectorRowsInput = {
   predicates: Record<string, ReviewServingProjectorRecordValue | readonly string[]>
-  table: ReviewServingProjectorWritableTable | 'mart.review_selected_article_import_current_v4'
+  table: ReviewServingProjectorWritableTable
 }
 
 export type RemoveReviewServingTitleSearchArticleIdsInput = {
